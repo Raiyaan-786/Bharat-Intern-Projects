9487c1574ff61d3173ff6bbf89c620d13db8bc1e
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-05-25 00:17:18 +0530

    Success and error pages refactored and css added

--- RegistrationForm/index.js
+++ RegistrationForm/index.js
@@ -11,20 +11,44 @@ const port = process.env.PORT || 3000 ;
 
 mongoose.connect(process.env.MONGO_URL);
 
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const Registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/' , (req,res) => {
     res.sendFile(__dirname + "/pages/index.html");
 })
 
-app.post('/register' , (req,res) => {
+app.post('/register' ,async (req,res) => {
     try{
         const { name,email,password } = req.body;
 
+        const registrationData = new Registration({
+            name,
+            email,
+            password
+        });
+        await registrationData.save();
+        res.redirect("/success");
     }
     catch(error){
-        
+        console.log(error);
+        res.redirect("/error");
     }
 })
-
+app.get('/success' , (req,res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+})
+app.get('/error' , (req,res) => {
+    res.sendFile(__dirname + "/pages/error.html");
+})
 app.listen(port, () => {
     console.log(`Server is Running on Port ${port}`);
 })
