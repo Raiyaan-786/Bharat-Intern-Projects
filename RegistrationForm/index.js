9996cf2194662e94ad86bfe9a914e3b719ccf240
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-05-24 23:53:14 +0530

    Css added in index.html file

--- RegistrationForm/index.js
+++ RegistrationForm/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 const port = process.env.PORT || 3000 ;
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/' , (req,res) => {
     res.sendFile(__dirname + "/pages/index.html");
 })
@@ -16,7 +18,7 @@ app.get('/' , (req,res) => {
 app.post('/register' , (req,res) => {
     try{
         const { name,email,password } = req.body;
-        
+
     }
     catch(error){
         
