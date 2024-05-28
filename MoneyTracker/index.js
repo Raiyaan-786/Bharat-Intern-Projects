e7c7f6c325b310676365606b662b35e5a3b8df7b
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-05-28 23:58:25 +0530

    Connected To MongoDB

--- MoneyTracker/index.js
+++ MoneyTracker/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000 ;
 
