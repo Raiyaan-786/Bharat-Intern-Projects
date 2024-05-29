2385c40eb167de2dc639021072bc702675df1f27
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-05-29 23:53:02 +0530

    Index.html form added

--- MoneyTracker/index.js
+++ MoneyTracker/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/MoneyTracker");
 const db = mongoose.connection
 db.on("error", ()=> console.log("Error in connecting to the Database"))
-db.on("open", ()=> console.log("Connected to Database"));
+db.once("open", ()=> console.log("Connected to Database"));
 
 const port = process.env.PORT || 3000 ;
 
