e0d998eded2a6032e8ca1b9064c4aa4a9f4755d5
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-06-02 23:21:11 +0530

    Frontend linked with backend

--- BlogWebsite/server.js
+++ BlogWebsite/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.get('/' , (req,res) => {
-    res.send("Welcome to Project");
+    res.render('index');
 })
 app.listen(3000);
