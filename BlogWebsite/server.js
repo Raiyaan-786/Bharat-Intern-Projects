42f6f307ce56441ecc5e6bd5257717f58071758a
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-06-02 23:27:54 +0530

    Routes created

--- BlogWebsite/server.js
+++ BlogWebsite/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const articleRouter = require('./routes/articles')
 const mongoose = require('mongoose');
 
 const app = express();
@@ -8,4 +9,7 @@ app.set('view engine', 'ejs');
 app.get('/' , (req,res) => {
     res.render('index');
 })
+
+app.use('/articles' , articleRouter);
+
 app.listen(3000);
