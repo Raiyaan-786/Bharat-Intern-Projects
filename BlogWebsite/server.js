570474b131f8b01eef8cc02261c8fe60e9984bc3
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2024-06-03 03:12:52 +0530

    Homepage created

--- BlogWebsite/server.js
+++ BlogWebsite/server.js
@@ -7,7 +7,19 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/' , (req,res) => {
-    res.render('index');
+    const articles = [
+        {
+            title: 'Test Article',
+            createdAt: new Date(),
+            description: 'Test Article Description'
+        },
+        {
+            title: 'Test Article 2',
+            createdAt: new Date(),
+            description: 'Test Article 2 Description'
+        },
+    ]
+    res.render('index',{articles:articles});
 })
 
 app.use('/articles' , articleRouter);
